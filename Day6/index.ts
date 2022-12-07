d2c4605fb7e2b206e6a5dc873c28b5173044004b
Tsubasa <tsubasanikander@gmail.com>
2022-12-07 09:28:33 +0100

    Init

---
+++ Day6/index.ts
@@ -0,0 +1,6 @@
+import { promises as fs } from 'fs';
+
+async function getData(): Promise<string[] | undefined> {
+    const rawData = await fs.readFile('./Day6/data.txt', 'utf8');
+    return rawData.split(/\r?\n/);
+}
